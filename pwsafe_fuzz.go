dc9e39de59c6bb90d544f75a4539da4fb484dbdf
Colin Newell <colin.newell@gmail.com>
2020-05-02 16:25:24 +0100

    Try to generate the fuzzer down a path of more valid data structures

--- pwsafe_fuzz.go
+++ pwsafe_fuzz.go
@@ -9,11 +9,110 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	fuzz "github.com/google/gofuzz"
+	"github.com/google/uuid"
 )
 
 func Fuzz(data []byte) int {
 	var pwFile V3File
-	fuzz.NewFromGoFuzz(data).Fuzz(&pwFile)
+	f := fuzz.NewFromGoFuzz(data).Funcs(
+		func(h *HeaderRecord, c fuzz.Continue) {
+			var typeID byte
+			c.Fuzz(&typeID)
+			*h = HeaderRecord{Type: typeID}
+			switch typeID {
+			case Version:
+				h.Data = "3.0" // FIXME:!
+			case UUID:
+				// uuid
+				var u uuid.UUID
+				c.Fuzz(&u)
+				h.Data = u
+			case TimestampOfLastSave, LastMasterPasswordChange:
+				// time_t
+				var t uint32
+				c.Fuzz(&t)
+				h.Data = t
+			case DatabaseDescription, DatabaseFilters, DatabaseName, EmptyGroups,
+				EndOfEntry, LastSavedByUser, LastSavedOnHost, NamedPasswordPolicies,
+				NondefaultPreferences, RecentlyUsedEntries, TreeDisplayStatus,
+				WhatPerformedLastSave, WhoPerformedLastSave, Yubico:
+				// string
+				var val string
+				c.Fuzz(&val)
+				(*h).Data = val
+			default:
+				var bytes []byte
+				c.Fuzz(&bytes)
+				h.Data = bytes
+			}
+		},
+		func(r *PasswordRecord, c fuzz.Continue) {
+			// FIXME: need to do a loop and create fields
+			// need to do some mandatory fields + some random
+			var password string
+			var user string
+			var u uuid.UUID
+			c.Fuzz(&password)
+			c.Fuzz(&user)
+			c.Fuzz(&u)
+			*r = PasswordRecord{
+				Fields: map[byte]Field{
+					UUID: Field{
+						Type: UUID,
+						Data: u,
+					},
+					Username: Field{
+						Type: Username,
+						Data: user,
+					},
+					Password: Field{
+						Type: Password,
+						Data: password,
+					},
+				},
+			}
+			// var typeID byte
+			// c.Fuzz(&typeID)
+
+			// switch typeID {
+			// case UUID:
+			// 	// uuid
+			// 	var err error
+			// 	var source [16]byte
+			// 	c.Fuzz(&source)
+			// 	data, err = uuid.FromBytes(source[:])
+			// 	if err != nil {
+			// 		panic(err)
+			// 	}
+			// 	r.Data = data
+			// case CreationTime, PasswordModificationTime,
+			// 	LastAccessTime, PasswordExpiryTime,
+			// 	LastModificationTime:
+			// 	// time_t
+			// 	var t uint32
+			// 	c.Fuzz(&t)
+			// 	r.Data = t
+			// case Group, Autotype, CreditCardExpiration,
+			// 	CreditCardNumber, CreditCardPIN,
+			// 	CreditCardVerifValue, EMailAddress, Notes,
+			// 	OwnSymbolsForPassword, Password, PasswordHistory,
+			// 	PasswordPolicy, PasswordPolicyName, QRCode,
+			// 	RunCommand, Title, URL, Username:
+			// 	// string
+			// 	var val string
+			// 	c.Fuzz(&val)
+			// 	r.Data = val
+			// default:
+			// 	// there are various types we know about that are
+			// 	// binary so we're letting them come here as well
+			// 	// as things we're not aware of
+			// 	var bytes []byte
+			// 	c.Fuzz(&bytes)
+			// 	r.Data = bytes
+			// }
+		},
+	)
+	f.Fuzz(&pwFile)
 	TestRoundTrip(pwFile)
 
 	return 0
